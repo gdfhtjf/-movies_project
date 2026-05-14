3bb2390329f46f44114f88d21afa6fc6435dd9c7
gdfhtjf <2208851320@qq.com>
2026-05-14 22:56:21 +0800

    Fix: @Primary DefaultCookieSerializer 强制 SameSite=None

--- backend/src/main/java/com/movie/config/WebMvcConfig.java
+++ backend/src/main/java/com/movie/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -24,6 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private String uploadDir;
 
     @Bean
+    @Primary
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setSameSite("None");
